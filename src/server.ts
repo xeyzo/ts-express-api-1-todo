1100247d144a85b49535b86a08271c5d12b42c0c
xeyzo <schneidergw@yahoo.com>
2020-12-07 09:02:29 +0700

    add nodemon

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import mongoose, { mongo } from 'mongoose';
 import { json } from 'body-parser';
 import { todoRouter } from './routes/todo';
 
+let port : number = 3000;
 const app = express();
 app.use(json());
 app.use(todoRouter);
 
 mongo.connect
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server running on port 3000')
 })
